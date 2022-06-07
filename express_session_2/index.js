effaffb602f1d5f7b1317b20f9b4aedf08539627
vuptgch17406 <vuptgch17406@fpt.edu.vn>
2022-06-07 11:42:16 +0700

    middleware

--- express_session_2/index.js
+++ express_session_2/index.js
@@ -11,6 +11,7 @@ app.get("/home", (req, res) => {
   res.send("This is my home");
 });
 app.use("/student", studentRouter);
+app.use("/teacher", teacherRouter);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
--- express_session_2/router/teacher.js
+++ express_session_2/router/teacher.js
@@ -1,30 +1,3 @@
-// step1: Tao router cho teacher
-// step2: Tao const dang Array luu 3 phan tu
-// [
-//   {
-//     id: 1,
-//     name: "Tuan",
-//     job: "teacher",
-//   },
-
-//   {
-//     id: 2,
-//     name: "Minh",
-//     job: "teacher",
-//   },
-
-//   {
-//     id: 3,
-//     name: "Vu",
-//     job: "teacher",
-//   },
-// ];
-
-// step3: tao method GET ==> return ve mang teacher
-// tao method POST ==> them phan tu vao mang teacher
-// PUT ==> update phan tu 2 ==> name: Khai, job: 'teacher-supporter'
-// DELETE xoa phan tu cuoi cung trong mang teacher
-
 const express = require("express");
 const teacherRouter = express.Router();
 
@@ -48,8 +21,38 @@ const teachers = [
   },
 ];
 
+// method GET
 teacherRouter.get("/", (req, res) => {
   res.json(teachers);
+});
+// method POST
+teacherRouter.post("/", (req, res) => {
+  let { id, name, job } = req.body;
+  // if (!name) {
+  //   name = "default name";
+  // }
+  teachers.push({
+    id,
+    name,
+    job,
+  });
+  res.status(201);
+  res.json(teachers);
+});
+// method PUT
+teacherRouter.put("/:id", (req, res) => {
+  teachers[req.params.id].name = "Khai";
+  teachers[req.params.id].job = "teacher-supporter";
+
+  res.json(teachers);
+});
+// method DELETE
+teacherRouter.delete("/:id", (req, res) => {
+  // push: them 1 phan tu vao cuoi array
+  // pop: lay ra phan tu cuoi array
+  teachers.splice(req.params.id, 1);
+  res.json(teachers);
+  res.send("Hello");
 });
 
 module.exports = teacherRouter;
