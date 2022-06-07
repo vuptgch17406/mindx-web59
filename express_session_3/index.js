c3fc1c369a42a75b1bde1f410971e904699368bb
vuptgch17406 <vuptgch17406@fpt.edu.vn>
2022-06-07 15:18:04 +0700

    middleware

--- express_session_3/index.js
+++ express_session_3/index.js
@@ -3,10 +3,11 @@ const app = express();
 const studentRouter = require("./router/students");
 const LoggingMiddleware = require("./middleware/logging_middleware");
 const studentMiddleware = require("./middleware/student_middleware");
+const morgan = require("morgan");
 const bodyParser = require("body-parser");
-const port = 3000;
-app.use(bodyParser.json({ extended: true }));
 
+app.use(morgan("combined"));
+app.use(bodyParser.json({ extended: true }));
 app.use("/student", LoggingMiddleware, studentMiddleware, studentRouter);
 
 app.get("/teacher", LoggingMiddleware, (req, res) => {
@@ -17,6 +18,7 @@ app.get("/class", LoggingMiddleware, (req, res) => {
   res.json("Success");
 });
 
+app.use(express.static("assests"));
 app.listen(3000, (err) => {
   if (err) {
     return;
