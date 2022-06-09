4e95e90b2586aab9e86c50ecd2dd89873261b954
vuptgch17406 <vuptgch17406@fpt.edu.vn>
2022-06-09 16:33:40 +0700

    update

--- express_session_3/context.md
+++
@@ -1,3 +0,0 @@
-Context: Co 1 array hoc sinh
-
-Tao 1 API chi approve cho mot hoc sinh trong array. Cac API khong co thong tin cua hoc sinh thi se bi chan
--- express_session_3/middleware/logging_middleware.js
+++
@@ -1,9 +0,0 @@
-function LoggingMiddleware(req, res, next) {
-  // request: gui len
-  // response: tra ve
-  // next: phuong thuc de day tien trinh den ham tiep theo
-  console.log("New request at", new Date());
-  next();
-}
-
-module.exports = LoggingMiddleware;
